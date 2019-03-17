c42ea354b8cde79435a5ba6357b0ddbf329b5daf
github username <suoarik@gmail.com>
2019-03-17 16:10:30 +0000

    added reducers, and default params

--- redux-playground/src/playground/redux-101.js
+++ redux-playground/src/playground/redux-101.js
@@ -1,56 +1,74 @@
 import { createStore } from "redux"
 
-const store = createStore((state = { count: 0 }, action ) => {
+// action generators, we use default object to avoid errors trying to get a property off undefined object
+const incrementCount = ({ incrementBy = 1 } = {}) =>  ({
+    type: "INCREMENT",
+    incrementBy: incrementBy
+  })
 
+  const decrementCount = ({ decrementBy = 1 } = {}) => ({
+      type: "DECREMENT",
+      decrementBy
+    })
+
+  const setCount = ({ count = 0 } = {}) => ({
+    type: "SET",
+    count
+  })
+
+  const resetCount = ({ reset = 0 } = {}) => ({
+    type: "RESET",
+    reset
+  })
+
+// Reducers are pure functions
+// 1. output only determined by the input
+// 2. Never change state or action
+
+// create store takes two arguments state, and action
+const countReducer = ((state = { count: 0 }, action ) => {
   switch (action.type) {
     case 'INCREMENT':
     return {
-      count: state.count + 1
+      count: state.count + action.incrementBy
     }
     break
     case 'DECREMENT':
       return {
-        count: state.count - 1
+        count: state.count - action.decrementBy
       }
       break
     case 'RESET':
       return {
-        count: state.count = 0
+        count: state.count = action.reset
+      }
+      break
+    case 'SET':
+      return {
+        count: action.count
       }
       break
     default:
       return state
   }
-
 })
 
-//returns the current state
-console.log(store.getState())
+const store = createStore(countReducer)
 
+// runs everytime the app changes, useful for updating page
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState())
+});
 
+// unsubscribe() call when you want to stop watching changes
 
 // Actions - an object that gets sent to the store
 // allows us to send of an action object to the store
-store.dispatch({
-  type: "INCREMENT" // convention actions to uppercase
-})
+store.dispatch(incrementCount({ incrementBy: 5 }))
 
-store.dispatch({
-  type: "INCREMENT" // convention actions to uppercase
-})
+store.dispatch(decrementCount({ decrementBy: 10 }))
 
-store.dispatch({
-  type: "RESET"
-})
+store.dispatch(setCount( {count: 101 }))
 
-store.dispatch({
-  type: "DECREMENT" // convention actions to uppercase
-})
+store.dispatch(resetCount())
 
-
-
-console.log(store.getState())
-
-
-// I'd like to increment the count
-// I'd like to reset the count to zero
